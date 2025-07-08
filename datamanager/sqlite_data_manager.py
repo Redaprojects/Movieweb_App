f26eba2c1eaf44c698756b69f207621ee4092a44
Red.prjects <red.me1aqw@gmail.com>
2025-07-08 18:05:17 +0200

    Add CRUD operations to SQLiteDataManager (users and movies)

--- datamanager/sqlite_data_manager.py
+++ datamanager/sqlite_data_manager.py
@@ -18,3 +18,62 @@ class SQLiteDataManager(DataManagerInterface):
 
         with app.app_context():
             db.create_all()
+
+
+    def get_all_users(self):
+        """Return all users in the database."""
+        with self.app.app_context():
+            return User.query.all()
+
+
+    def get_user_movies(self, user_id):
+        """Return all movies for the given user."""
+        with self.app.app_context():
+            return Movie.query.filter_by(user_id=user_id).all()
+
+
+    def add_user(self, user):
+        """Add a new user to the database."""
+        with self.app.app_context():
+            new_user = User(name=user['name'])
+            db.session.add(new_user)
+            db.session.commit()
+            return new_user
+
+
+    def add_movie(self, user_id, movie):
+        """Add a new movie for a specific user."""
+        with self.app.app_context():
+            new_movie = Movie(
+                name=movie['name'],
+                director=movie['director'],
+                year=movie['year'],
+                rating=movie['rating'],
+                user_id=user_id
+            )
+            db.session.add(new_movie)
+            db.session.commit()
+            return new_movie
+
+
+    def update_movie(self, movie_id, updated_data):
+        """Update an existing movie's fields."""
+        with self.app.app_context():
+            movie = Movie.query.get(movie_id)
+            if not movie:
+                return None
+            for key, value in updated_data.items():
+                setattr(movie, key, value)
+            db.session.commit()
+            return movie
+
+
+    def delete_movie(self, movie_id):
+        """Delete a movie from the database."""
+        with self.app.app_context():
+            movie = Movie.query.get(movie_id)
+            if movie:
+                db.session.delete(movie)
+                db.session.commit()
+                return True
+            return False
